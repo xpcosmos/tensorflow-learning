cc411805b8f8c300b312da8868f53e463d9facbf
xpcosmos <mikeias.d.s.o@gmail.com>
2023-11-05 15:00:17 -0300

    Method 1 to build a neural network

--- first_neural_network/ffn.py
+++ first_neural_network/ffn.py
@@ -12,3 +12,24 @@ print(x_train.shape, y_train.shape)
 
 # Normalize data
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
+for i in range(6):
+    plt.subplot(2, 3, i+1)
+    plt.imshow(x_train[i], cmap='gray')
+plt.show()
+
+# Method 1
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)), # Converte um array de (28, 28) em um Array (784, 1)
+    keras.layers.Dense(128, activation='relu'), # Camada escondida com interconectada função de ativação ReLu
+    keras.layers.Dense(10) # Camada de output, igual numero de classes de predição
+])
+
+print(model.summary())
+
+# Method 2
+
+model = keras.Sequential
+model.add(keras.layers.Flatten(input_shape=(28,28)))
+model.add(keras.layers.Dense(128, activation='relu'))
+mo

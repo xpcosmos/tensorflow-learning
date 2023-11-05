a53ae9a4a6d6b03f776e288e24332f22955f627f
xpcosmos <mikeias.d.s.o@gmail.com>
2023-11-05 15:15:20 -0300

    Fitting Data to the model

--- first_neural_network/ffn.py
+++ first_neural_network/ffn.py
@@ -20,16 +20,34 @@ plt.show()
 
 # Method 1
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28,28)), # Converte um array de (28, 28) em um Array (784, 1)
-    keras.layers.Dense(128, activation='relu'), # Camada escondida com interconectada função de ativação ReLu
-    keras.layers.Dense(10) # Camada de output, igual numero de classes de predição
+    keras.layers.Flatten(input_shape=(28,28)), # Convert a array (28,28) to (784,1)
+    keras.layers.Dense(128, activation='relu'), # Hidden layer interconected and with ReLu Activation function
+    keras.layers.Dense(10) # Output layer. Number of neurons is the same as the number of categories 
 ])
 
 print(model.summary())
 
 # Method 2
 
-model = keras.Sequential
-model.add(keras.layers.Flatten(input_shape=(28,28)))
-model.add(keras.layers.Dense(128, activation='relu'))
-mo
+# model = keras.Sequential()
+# model.add(keras.layers.Flatten(input_shape=(28,28)))
+# model.add(keras.layers.Dense(128, activation='relu'))
+# model.add(keras.layers.Dense(10))
+
+# print(model.summary())
+
+# from_logits is true because the NN doesn't have SoftMax layer
+loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+# Adam optimization is a stochastic gradient descent method that is based on adaptive estimation of first-order and second-order moments.
+optimizer = keras.optimizers.Adam(lr=0.001) # lr = Learning Rate
+metrics = ['accuracy']
+
+model.compile(loss=loss, metrics=metrics, optimizer=optimizer)
+
+# Number of samples processed before the model is updated
+BATCH_SIZE = 64
+# Number of complete passes through the training dataset
+EPOCHS = 5
+
+# Fitting model to the train data
+model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, shuffle=True, verbose=1) 
